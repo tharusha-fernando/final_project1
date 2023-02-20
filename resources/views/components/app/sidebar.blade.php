98119760d93bb5c62e26a95c25c4ec6a18d3b9b4
Tharusha Fernando <tharushafdo2000@gmail.com>
2023-02-20 13:06:31 +0530

    Initial Release

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -446,7 +446,8 @@
                                     </svg>
                                     <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">New Employee</span>
                                 </div>
-                            </a>
+                            </a><!---->
+                            <!---->
                         </li>
                         <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0 @if(in_array(Request::segment(1), ['calendar'])){{ 'bg-slate-900' }}@endif">
                             <a class="block text-slate-200 hover:text-white truncate transition duration-150 @if(in_array(Request::segment(1), ['calendar'])){{ 'hover:text-slate-200' }}@endif" href="/contact_usUs">
